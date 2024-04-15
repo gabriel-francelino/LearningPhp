eee91719376b68d6495aaf20e3f3395851154529
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-15 17:55:57 -0300

    hello world com php

---
+++ ex000-hello-world/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            // Exibe uma mensagem de texto
+            // Comandos para imprimir: echo, print, print_r
+            // tem ';' no final
+            echo "Hello World! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Me livrando da maldição</p>
+</body>
+</html>

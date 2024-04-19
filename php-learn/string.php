37e5690476c9eee0254a4e1b5d22149f9f1a26fd
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-19 09:01:25 -0300

    corrigindo erros no heredoc e nowdoc

--- php-learn/string.php
+++ php-learn/string.php
@@ -15,16 +15,31 @@ $oranges  =  "Tenho ${number} laranjas para comer.";  // => Tenho 23 laranjas pa
 $money  =  "Tenho $ ${number} no banco.";      // => Tenho $23 no banco. (depreciado)
 
 // Desde o PHP 5.3, nowdocs podem ser usados ​​para multi-liners não interpolados 
-// $nowdoc  =  <<<' END ' 
-// Multi line 
-// string 
-// END ;
+$nowdoc = <<<'END'
+    Multi line 
+    string 
+END;
 
 // Heredocs fará interpolação de strings 
-// $heredoc  =  <<< END 
-// Multi line 
-// $sgl_quotes 
-// END ;
+$heredoc1 = <<<OI
+    Multiline 
+    $string 
+OI;
+
+$heredoc2 = <<<END
+END;
+
+$heredoc = <<<END
+    Multi line
+    $string
+END;
+
+$html = <<<HTML
+  	<div id="ola">
+	    <p>Olá {$seu_nome},</p>
+    	<p>Que tal visitar a gente em nossa sede: {$endereco}?</p>
+  	</div>
+HTML;
 
 // Concatenacao de strings
 $string1 = 'primeira parte da string';

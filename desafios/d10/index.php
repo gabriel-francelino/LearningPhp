3ebfce663e8891ea440c1e1d7f210939b981c523
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-04-25 16:27:22 -0300

    add desafios

---
+++ desafios/d10/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 10</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php 
+        $ano_atual = (int) date('Y');
+        $ano_nasc = $_GET['ano_nasc'] ?? $ano_atual;
+        $ano_idade = $_GET['ano_idade'] > $ano_nasc ? $_GET['ano_idade'] : $ano_atual;
+    ?>
+    <main>
+        <h1>Calculando a sua idade</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="ano_nasc">Em que ano você nasceu?</label>
+            <input type="number" name="ano_nasc" id="ano_nasc" value="<?=$ano_nasc?>">
+            <label for="ano_idade">Quer saber sua idade em que ano?</label>
+            <input type="number" name="ano_idade" id="ano_idade" value="<?=$ano_idade?>">
+            <input type="submit" value="Qual seá minha idade? (Atualmente estamos em <?=$ano_atual?>)">
+        </form>
+    </main>
+    <section id="resultado">
+        <h2>Resultado</h2>
+        <?php
+            $idade = $ano_idade - $ano_nasc;
+            echo "<p>Quem nasceu em $ano_nasc vai ter <strong>$idade anos</strong> em $ano_idade!</p>";
+        ?>
+    </section>
+</body>
+
+</html>
---
+++ desafios/d11/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 11</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php 
+        $preco = $_GET['preco'] ?? 0.00;
+        $porcentagem = $_GET['porcentagem'] ?? 0.00;
+    ?>
+    <main>
+        <h1>Reajustador de preços</h1>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <label for="preco">Preço do produto (R$)</label>
+            <input type="number" name="preco" id="preco" step="0.01" value="<?=$preco?>">
+            <label for="porcentagem">Qual será o porcentual de reajuste? (<strong><?=$porcentagem*100?>%</strong>)</label>
+            <input type="range" name="porcentagem" id="porcentagem" min="0" max="1" step="0.01" value="<?=$porcentagem?>">
+            <input type="submit" value="Reajustar">
+        </form>
+    </main>
+    <section id="resultado">
+        <h2>Resultado do Reajuste</h2>
+        <?php
+            function format($valor) {
+                return number_format($valor, 2, ',', '.');
+            };
+
+            $reajuste = $preco * (1 + $porcentagem);
+            echo "<p>O produto que custava R$" . format($preco) . ", com " . ($porcentagem*100) . "% de aumento vai passar a custar R$" . format($reajuste) . " a partir de agora.</p>";
+        ?>
+    </section>
+</body>
+
+</html>
